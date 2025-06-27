9c760cf74043b6f8977a7fa1942e795900b4cd3d
thanglp163 <lethangtty2018@gmail.com>
2025-06-28 00:11:56 +0700

    feat: Intergrate

--- src/lib/services/user.ts
+++ src/lib/services/user.ts
@@ -1,4 +1,5 @@
 const USER_BASE_URL = `${process.env.BACKEND_BASE_URL}/user`;
+const USER_API_URL = `${process.env.NEXT_PUBLIC_SERVER_URI}/users`;
 
 export async function getUserByEmail(email: string) {
     try {
@@ -29,5 +30,16 @@ export async function createUser({ name, email, image }: { name: string; email: 
         return user;
     } catch {
         throw new Error('Could not create user');
+    }
+}
+
+export async function getUserById(id: string) {
+    try {
+        const res = await fetch(`${USER_API_URL}/${id}`);
+        if (!res.ok) throw new Error('Failed to fetch user by ID');
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        throw new Error('Could not fetch user by ID');
     }
 }
