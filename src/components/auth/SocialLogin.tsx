1331c72fe4803e914edd409bb032967ee9867d96
Manh Hung <hungoffsize@gmail.com>
2025-08-12 15:55:36 +0700

    fix: remove Facebook icon

--- src/components/auth/SocialLogin.tsx
+++ src/components/auth/SocialLogin.tsx
@@ -1,14 +1,26 @@
 import Image from 'next/image';
 import { signIn } from 'next-auth/react';
 
-const SocialLogin = () => (
+type SocialLoginProps = {
+  showFacebook?: boolean;
+};
+
+const SocialLogin = ({ showFacebook = true }: SocialLoginProps) => (
   <div className="flex justify-center gap-4 mt-2">
     <button className="bg-white p-3 rounded-full hover:bg-gray-200 cursor-pointer">
-      <Image src="/assets/home/Google.svg" alt="Google" width={16} height={16} onClick={() => signIn('google')} />
+      <Image
+        src="/assets/home/Google.svg"
+        alt="Google"
+        width={16}
+        height={16}
+        onClick={() => signIn('google')}
+      />
     </button>
-    <button className="bg-white p-3 rounded-full hover:bg-gray-200 cursor-pointer">
-      <Image src="/assets/home/facebook.svg" alt="Google" width={18} height={18} />
-    </button>
+    {showFacebook && (
+      <button className="bg-white p-3 rounded-full hover:bg-gray-200 cursor-pointer">
+        <Image src="/assets/home/facebook.svg" alt="Facebook" width={18} height={18} />
+      </button>
+    )}
   </div>
 );
 export default SocialLogin;
